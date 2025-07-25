03c8f22ccccc920f1eed409546be8f86b4f57b97
Sriram Suresh <sriram.suresh449@gmail.com>
2025-07-24 17:17:50 -0700

    added statement to help debug

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -7,6 +7,7 @@ def download_pdf(url, out_path):
         f.write(resp.content)
 
 def extract_chunks(pdf_path, chunk_size=1000):
+    print(f"\nExtracting chunks from {pdf_path} \n")
     doc = fitz.open(pdf_path)
     text = "\n".join(page.get_text() for page in doc)
     return [text[i : i+chunk_size] for i in range(0, len(text), chunk_size)] 
